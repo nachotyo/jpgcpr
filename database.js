59725c1c3f8434cc97b7e62a16248de22549e40b
nachotyo <74370989+nachotyo@users.noreply.github.com>
2022-06-06 20:37:13 +0200

    Delete database.js

--- database.js
+++
@@ -1,9 +0,0 @@
-// Creamos la base de datos
-const mongoose = require('mongoose');
-// Obetenemos el objeto mongodb
-const { mongodb } = require('./keys');
-
-// Nos conectamos a la base de datos y mostramos por consola si se ha conectado
-mongoose.connect(mongodb.URI, {useNewUrlParser: true})
-.then(db => console.log('La base de datos esta conectada'))
-.catch(err => console.log(err));
